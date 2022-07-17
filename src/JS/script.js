dcfebb8e6e7ae3374151e427c9a076618352029f
Serhii <grammas56@gmail.com>
2022-07-17 14:29:58 +0300

    pulsators_carousel

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,18 +1,12 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.png"></button>',
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                dots: true,
-                arrows: false
-              }
-            }
-        ]
-    });
+  $('.carousel__inner').slick({
+    dots: false,
+    infinite: true,
+    speed: 1200,
+    slidesToShow: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.png"></img></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.png"></img></button>',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
 });
-
