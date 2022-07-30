d786de40bb5fed9968d277228df9684ade4eff99
Serhii <grammas56@gmail.com>
2022-07-30 01:52:44 -0700

    pulsators_modal

--- src/JS/script.js
+++ src/JS/script.js
@@ -36,4 +36,28 @@ $(document).ready(function(){
 
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
+
+// Modal
+
+  $('[data-modal=consultation]').on('click', function(){
+    $('.overlay, #consultation').fadeIn('fast');
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+  });
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+    })
 });
+
+  $('.button_submit').on('click', function(){
+    $('.overlay, #thanks').fadeIn('fast');
+    $('#consultation, #order').fadeOut('slow');
+  });
+
+});
+
