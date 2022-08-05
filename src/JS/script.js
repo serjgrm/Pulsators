c52070d1cf03408601765bb80b31e427018c61c9
Serhii <grammas56@gmail.com>
2022-08-05 11:43:12 -0700

    pulsators_scroll

--- src/JS/script.js
+++ src/JS/script.js
@@ -106,5 +106,23 @@ $('form').submit(function(e) {
   });
   return false;
 });
+
+// smooth scroll and pageup
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+      } else {
+      $('.pageup').fadeOut();
+      }
+  });
+
+  $("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+new WOW().init();
 });
 
